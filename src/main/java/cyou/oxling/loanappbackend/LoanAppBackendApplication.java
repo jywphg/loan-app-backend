090774f7a0dde7a23c76e9a556cfda0cb9847281
Chanler <merryfling@gmail.com>
2025-05-15 20:30:05 +0800

    fix: 修复推送功能配置问题

--- src/main/java/cyou/oxling/loanappbackend/LoanAppBackendApplication.java
+++ src/main/java/cyou/oxling/loanappbackend/LoanAppBackendApplication.java
@@ -1,14 +1,21 @@
 package cyou.oxling.loanappbackend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class LoanAppBackendApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
+		
+		// 将.env中的变量设置为系统属性，以便Spring能够识别
+		System.setProperty("GETUI_APPID", dotenv.get("GETUI_APPID"));
+		System.setProperty("GETUI_APPKEY", dotenv.get("GETUI_APPKEY"));
+		System.setProperty("GETUI_MASTERSECRET", dotenv.get("GETUI_MASTERSECRET"));
+		
 		SpringApplication.run(LoanAppBackendApplication.class, args);
 	}
 
